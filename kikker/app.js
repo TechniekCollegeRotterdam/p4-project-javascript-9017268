6d6764467837b697dea5e66170553e16427cb093
Esra Adsoy <9017268@student.tcrmbo.nl>
2022-06-08 23:23:34 +0200

    .

--- kikker/app.js
+++ kikker/app.js
@@ -1,4 +1,7 @@
-setTimeout(function () {alert('game tijd voorbij'); location.reload(true);}, 5000);
+setTimeout(function () {
+        alert('game tijd voorbij');
+        location.reload(true);
+}, 100000);
 
 const nameChanged = function (event) {
         console.log(event);
@@ -6,96 +9,67 @@ const nameChanged = function (event) {
 
 const kikker = document.querySelector(".kikker");
 
+
 let px = 0;
-
-addEventListener("click", function () {
-       px = px + 50;
-
-       kikker.style.left = px + "px";
-
-
-       //console.log("geklikt");
-})
-
 
 
 window.addEventListener("keydown", function (event) {
         console.log(event.key);
 
         if (event.key == "ArrowRight") {
-                px = px + 20;
+
+                px = px + 10;
 
                 kikker.style.left = px + "px";
-                kikker.src = "images/kikker.png";
 
         }
 
         if (event.key == "ArrowLeft") {
-                px = px - 20;
+                px = px - 10;
 
                 kikker.style.left = px + "px";
-                kikker.src = "images/kikker.png";
         }
 
         if (event.key == "ArrowUp") {
-                px = px + 20;
+                px = px + 10;
 
                 kikker.style.bottom = px + "px";
-                kikker.src = "images/kikker.png";
         }
 
         if (event.key == "ArrowDown") {
-                px= px - 20;
+                px = px - 10;
 
                 kikker.style.bottom = px + "px";
-                kikker.src = "images/kikker.png";
+        }
+
+        if (event.key == "Enter") {
+
+                px = px + 50; // als je op enter drukt gaat hij 40 pixels sneller naar rechts
+
+                kikker.style.left = px + "px";
+        }
+
+        if (event.key == "Backspace") {
+
+                px = px - 50; //als je  op backspace drukt gaat hij 40 pixels sneller naar links
+
+                kikker.style.left = px + "px";
+        }
+
+        if (event.key == "Shift") {
+
+                px = px + 50; //als je op shift drukt gaat hij 40 pixels sneller naar boven
+
+                kikker.style.bottom = px + "px";
+
+        }
+
+        if (event.key == "p") {
+
+                px = px - 50; //als je op "P" drukt gaat hij 40 pixels sneller naar beneden
+
+                kikker.style.bottom = px + "px";
+
         }
 
 })
-
-
-function random_food(min, max)
-{  
-   return Math.round((Math.random() * (max-min) + min) / 10) * 10;
-}
- 
-  //function change_direction moet je veranderen!!
-function change_direction(event) 
-{  
-   const LEFT_KEY = 37;
-   const RIGHT_KEY = 39;
-   const UP_KEY = 38;
-   const DOWN_KEY = 40;
- 
-   const keyPressed = event.keyCode;
-   const goingUp = dy === -10;
-   const goingDown = dy === 10;
-   const goingRight = dx === 10;  
-   const goingLeft = dx === -10;
- 
-
-   //if keypressed moet je veranderen!!
-     if (keyPressed === LEFT_KEY && !goingRight)
-     {    
-          dx = -10;
-          dy = 0;  
-     }
- 
-     if (keyPressed === UP_KEY && !goingDown)
-     {    
-          dx = 0;
-          dy = -10;
-     }
- 
-     if (keyPressed === RIGHT_KEY && !goingLeft)
-     {    
-          dx = 10;
-          dy = 0;
-     }
- 
-     if (keyPressed === DOWN_KEY && !goingUp)
-     {    
-          dx = 0;
-          dy = 10;
-     }
-}
