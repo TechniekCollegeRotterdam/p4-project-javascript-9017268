03e461f9e4e39de464ed2e9ced237ddcb1f52641
Esra Adsoy <9017268@student.tcrmbo.nl>
2022-06-15 09:17:38 +0200

    progressie

--- kikker/app.js
+++ kikker/app.js
@@ -1,31 +1,10 @@
-setTimeout(function () {
-        alert('game tijd voorbij');
-        location.reload(true);
-}, 100000);
-
-const nameChanged = function (event) {
-        console.log(event);
-}
-
-const kikker = document.querySelector(".kikker");
-
-
-let px = 0;
-
-
 window.addEventListener("keydown", function (event) {
         console.log(event.key);
 
-        if (event.key == "ArrowRight") {
 
-                px = px + 10;
 
-                kikker.style.left = px + "px";
 
-        }
 
-        if (event.key == "ArrowLeft") {
-                px = px - 10;
 
                 kikker.style.left = px + "px";
         }
@@ -33,7 +12,6 @@ window.addEventListener("keydown", function (event) {
         if (event.key == "ArrowUp") {
                 px = px + 10;
 
-                kikker.style.bottom = px + "px";
         }
 
         if (event.key == "ArrowDown") {
@@ -44,7 +22,6 @@ window.addEventListener("keydown", function (event) {
 
         if (event.key == "Enter") {
 
-                px = px + 50; // als je op enter drukt gaat hij 40 pixels sneller naar rechts
 
                 kikker.style.left = px + "px";
         }
