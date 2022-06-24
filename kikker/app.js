1a0ba2487b1863aee46e736b59344b829eb61063
Esra Adsoy <9017268@student.tcrmbo.nl>
2022-06-24 14:55:06 +0200

    bijna af

--- kikker/app.js
+++ kikker/app.js
@@ -8,20 +8,27 @@ class SnakePart {
   }
 }
 
-let speed = 200; //dit is de snelheid , eerst was dit 7, dit moet nog veranderen
-let tileCount = 30; //dit is de grootte, was eerst 20
-let tilecount = 200;
-let tileSize = canvas.width / tileCount - 2;
-let tileSizeKikker = canvas.width / tilecount - 2;
+let speed = 14; //dit is de snelheid , eerst was dit 7, dit moet nog veranderen
+let tileCount = 10; //dit is de grootte
+let pos1 = 10; // voor de random positie vlieg
+let pos2 = 20; // voor de random positie mier
+let pos3 = 30; // voor de random positie lieveheersbeestje
+let tileSize = canvas.width / tileCount;
 
 let kikkerX = 10;
 let kikkerY = 10;
 
+let vliegX = 20;
+let vliegY = 20;
+
 let snakeParts = []; //array met snake parts (oftewel een lijstje)
 let tailLength = 2;
 
-let appleX = 5;
-let appleY = 5;
+let liefX = 30;
+let liefY = 30;
+
+let mierX = 40;
+let mierY = 40;
 
 let inputsXVelocity = 0;
 let inputsYVelocity = 0;
@@ -31,7 +38,7 @@ let yVelocity = 0;
 
 let score = 0;
 
-//const gulpSound = new Audio("gulp.mp3"); nieuwe audio vinden en erin zetten
+const gulpSound = new Audio("sound.mp3");
 
 //game loop
 function drawGame() {
@@ -52,10 +59,10 @@ function drawGame() {
   drawScore();
 
   if (score > 5) {
-    speed = 9;
+    speed = 10;
   }
   if (score > 10) {
-    speed = 11;
+    speed = 20;
   }
 
   setTimeout(drawGame, 1000 / speed);
@@ -68,14 +75,16 @@ function isGameOver() {
     return false;
   }
 
-  console.log(kikkerY);
-
-  if (kikkerX <= 0 || kikkerX > canvas.width) {
-    console.log("dit was de muur lul")
+  //walls
+  //eerst had ik canvas.width en canvas.height ,maar toen zag ik dat het met de tailcount te maken had en zo heb ik de code veranderd. 
+  //80-5, net niet van de canvas af. Ook het zelfde verhaal met de andere coordinaten.
+  if (kikkerX <= -5 || kikkerX > 75) { //width 800
     gameOver = true;
-  } else if (kikkerY <= 0 || kikkerY > canvas.height) {
-    gameOver = true; }
+  } else if (kikkerY <= -5 || kikkerY > 55) { //height 600
+    gameOver = true;
+  }
 
+  console.log(canvas.witdh);
 
   for (let i = 0; i < snakeParts.length; i++) {
     let part = snakeParts[i];
@@ -94,9 +103,8 @@ function isGameOver() {
       ctx.font = "50px Verdana";
 
       var gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
-      gradient.addColorStop("0", " magenta");
-      gradient.addColorStop("0.5", "blue");
-      gradient.addColorStop("1.0", "red");
+      gradient.addColorStop("0", " white");
+
       // Fill with gradient
       ctx.fillStyle = gradient;
 
@@ -116,23 +124,17 @@ function drawScore() {
 }
 
 function clearScreen() {
-  ctx.fillStyle = "rgb(254, 140, 243)";
+  ctx.fillStyle = "orange"
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function drawSnake() {
-  ctx.fillStyle = "green";
   //let part = snakeParts[0];
   let img = document.getElementById("kikker");
-  //ctx.drawImage(img, part.x, part.y);
   for (let i = 0; i < snakeParts.length; i++) {
-    let part = snakeParts[i];
-    let parts = part.x;
-    let partss = part.y;
-    //ctx.fillRect(part.x * tileCount, part.y * tileCount, tileSize, tileSize);
-    ctx.drawImage(img, parts, partss, tileCount, tileCount);
-    //ctx.drawImage(img);
-
+    //let part = snakeParts[i];
+    //ctx.drawImage(img, parts, partss, tileCount, tileCount);
+    ctx.drawImage(img, kikkerX * tileCount, kikkerY * tileCount, tileSize, tileSize);
   }
 
   snakeParts.push(new SnakePart(kikkerX, kikkerY)); //put an item at the end of the list next to the head
@@ -140,10 +142,7 @@ function drawSnake() {
     snakeParts.shift(); // remove the furthet item from the snake parts if have more than our tail size.
   }
 
-  //ctx.fillStyle = 'color';
-  //ctx.drawImage(img, 30, 30);
-  ctx.fillRect(kikkerX * img, kikkerY * img, tileSizeKikker, tileSizeKikker);
-  //ctx.fillRect( img, headX * tileCount, headY * tileCount, tileSize, tileSize);
+  ctx.fillRect(kikkerX * img, kikkerY * img, tileSize, tileSize);
 }
 
 function changeSnakePosition() {
@@ -151,33 +150,59 @@ function changeSnakePosition() {
   kikkerY = kikkerY + yVelocity;
 }
 
-function drawApple() {
-let vlieg = document.getElementById("vlieg");
-  ctx.fillStyle = "red";
-  ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
-  //ctx.fillRect(vlieg, appleX * tileCount, appleY * tileCount, tileSize, tileSize);
-  //ctx.drawImage(vlieg);
-}
 
-//function checkAppleCollision() {
-//  if (appleX === headX && appleY == headY) {
-//    appleX = Math.floor(Math.random() * tileCount);
-//    appleY = Math.floor(Math.random() * tileCount);
-//    tailLength++;
-//    score++;
-    //  gulpSound.play();
-//  }
-//}
+//console.log(vlieg);
+function drawApple() {
+  let vlieg = document.getElementById("vlieg");
+  let vlieg2 = new Image()
+  vlieg2.src = "./images/vlieg.png";
+
+  let mier = document.getElementById("mier");
+  let mier2 = new Image()
+  mier2.src = "./images/mier.png";
+
+  let lief = document.getElementById("lieveheersbeestje");
+  let lieve2 = new Image()
+  lieve2.src = "./images/lieveheersbeestje.png";
+
+
+  //ctx.fillStyle = "pink";
+  //ctx.fillRect(appleX * tileCount, appleY * tileCount, tileSize, tileSize);
+  // ctx.drawImage(vlieg, vliegX * tileCount, vliegY * tileCount, tileSize, tileSize);
+  ctx.drawImage(vlieg2, vliegX * tileCount, vliegY * tileCount, tileSize, tileSize);
+  ctx.drawImage(mier2, mierX * tileCount, mierY * tileCount, tileSize, tileSize);
+  ctx.drawImage(lieve2, liefX * tileCount, liefY * tileCount, tileSize, tileSize);
+
+
+
+  //ctx.drawImage(vlieg);
+  //ctx.fillRect(vlieg * appleX, vlieg * appleY, tileSize, tileSize);
+
+}
 
 function checkAppleCollision() {
-  if (appleX === kikkerX && appleY == kikkerY) {
-    appleX = Math.floor(Math.random() * tileCount);
-    appleY = Math.floor(Math.random() * tileCount);
+  if (vliegX === kikkerX && vliegY === kikkerY) {
+    vliegX = Math.floor(Math.random() * pos1);
+    vliegY = Math.floor(Math.random() * pos1);
     tailLength++;
     score++;
-    //  gulpSound.play();
+    gulpSound.play();
+  } else if (mierX === kikkerX && mierY === kikkerY) {
+    mierX = Math.floor(Math.random() * pos2);
+    mierY = Math.floor(Math.random() * pos2);
+    tailLength++;
+    score++;
+    gulpSound.play();
+  } else if (liefX === kikkerX && liefY === kikkerY) {
+    liefX = Math.floor(Math.random() * pos3);
+    liefY = Math.floor(Math.random() * pos3);
+    tailLength++;
+    score++;
+    gulpSound.play();
   }
+
 }
+
 
 let px = 0;
 
@@ -186,10 +211,10 @@ window.addEventListener("keydown", function (event) {
 
   if (event.key == "ArrowRight") {
 
-    px = px + 10;
+    px = px + speed;
 
-    kikker.style.left = px + "px";
-    //68 is d
+    // kikker.style.left = px + "px";
+
     inputsXVelocity = 1;
     if (inputsXVelocity == -1) return;
     inputsYVelocity = 0;
@@ -197,10 +222,10 @@ window.addEventListener("keydown", function (event) {
   }
 
   if (event.key == "ArrowLeft") {
-    px = px - 10;
+    px = px - speed;
 
-    kikker.style.left = px + "px";
-    // 65 is a
+    // kikker.style.left = px + "px";
+
     if (inputsXVelocity == 1) return;
     inputsYVelocity = 0;
     inputsXVelocity = -1;
@@ -208,19 +233,19 @@ window.addEventListener("keydown", function (event) {
   }
 
   if (event.key == "ArrowUp") {
-    px = px + 10;
+    px = px + speed;
 
-    kikker.style.bottom = px + "px";
-    //87 is w
+    // kikker.style.bottom = px + "px";
+
     if (inputsYVelocity == 1) return;
     inputsYVelocity = -1;
     inputsXVelocity = 0;
   }
 
   if (event.key == "ArrowDown") {
-    px = px - 10;
+    px = px - speed;
 
-    kikker.style.bottom = px + "px";
+    // kikker.style.bottom = px + "px";
     //83 is s
     if (inputsYVelocity == -1) return;
     inputsYVelocity = 1;
